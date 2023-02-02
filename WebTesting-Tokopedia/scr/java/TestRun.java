0c1a75e8e77b6abe0b2abfe81aaf280b03111c32
Fattah Widjaya Gandhi <Caplin404@gmail.com>
2023-02-03 01:58:46 +0700

    Add files via upload

---
+++ WebTesting-Tokopedia/scr/java/TestRun.java
@@ -0,0 +1,11 @@
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "classpath:features/",
+        plugin = "pretty"
+)
+public class TestRun {
+}
